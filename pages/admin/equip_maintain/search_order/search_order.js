99999b5fb6dfea9f409bd9358b9abd769c55fa19
cangjie <cangjie@hotmail.com>
2020-07-27 19:20:08 +0800

    search order

--- pages/admin/equip_maintain/search_order/search_order.js
+++ pages/admin/equip_maintain/search_order/search_order.js
@@ -8,7 +8,14 @@ Page({
   data: {
     tabbarItemList: [],
     tabIndex: 0,
-    waybillNo: ''
+    waybillNo: '',
+    files: [{
+      url: 'http://mmbiz.qpic.cn/mmbiz_png/VUIF3v9blLsicfV8ysC76e9fZzWgy8YJ2bQO58p43Lib8ncGXmuyibLY7O3hia8sWv25KCibQb7MbJW3Q7xibNzfRN7A/0',
+    }, {
+      loading: true
+    }, {
+      error: true
+    }]
   },
 
   /**
@@ -16,6 +23,10 @@ Page({
    */
   onLoad: function (options) {
     this.setData({tabbarItemList: app.globalData.adminTabbarItem})
+    this.setData({
+      selectFile: this.selectFile.bind(this),
+      uplaodFile: this.uplaodFile.bind(this)
+    })
   },
 
   /**
@@ -86,5 +97,47 @@ Page({
   },
   waybillNoInput: function(e){
     this.setData({waybillNo: e.detail.value})
-  }
+  },
+  clickOrder: function(e) {
+    console.log(e)
+  },
+  chooseImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: function (res) {
+            // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+            that.setData({
+                files: that.data.files.concat(res.tempFilePaths)
+            });
+        }
+    })
+},
+previewImage: function(e){
+    wx.previewImage({
+        current: e.currentTarget.id, // 当前显示图片的http链接
+        urls: this.data.files // 需要预览的图片http链接列表
+    })
+},
+selectFile(files) {
+    console.log('files', files)
+    // 返回false可以阻止某次文件上传
+},
+uplaodFile(files) {
+    console.log('upload files', files)
+    // 文件上传的函数，返回一个promise
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({'urls':['https://www.a.com/b.jpg']})
+            //reject('some error')
+        }, 1000)
+    })
+},
+uploadError(e) {
+    console.log('upload error', e.detail)
+},
+uploadSuccess(e) {
+    console.log('upload success', e.detail)
+}
 })
