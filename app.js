cb5f8d9ad07b4ee7d7d702bf72b7c8e3ed56bced
cangjie <cangjie@hotmail.com>
2020-08-19 13:52:02 +0800

    finis current surface

--- app.js
+++ app.js
@@ -2,6 +2,32 @@
 var crypto  = require('/utils/crypt.js')
 App({
   onLaunch: function () {
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+    })
+
+
+
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -25,7 +51,7 @@ App({
               this.globalData.role = res.data.role
               if (this.globalData.role == 'staff') {
                 wx.navigateTo({
-                  url: '/pages/admin/equip_maintain/search_order/search_order',
+                  url: '/pages/admin/equip_maintain/order_list/order_list_main',
                 })
               }
             }
