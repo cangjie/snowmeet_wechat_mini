4fc53d06903b59f8ad97e2dd54c2c23932564f79
cangjie <cangjie@hotmail.com>
2020-09-07 16:49:08 +0800

    add md5

--- pages/admin/equip_maintain/order_list/order_list_main.js
+++ pages/admin/equip_maintain/order_list/order_list_main.js
@@ -1,6 +1,6 @@
 // pages/admin/equip_maintain/order_list/order_list_main.js
 var wxloginModule = require('../../../../utils/wxlogin.js')
-require('../../../../utils/md5.js')
+var md5 = require('../../../../utils/md5.js')
 const app = getApp()
 Page({
 
@@ -18,6 +18,10 @@ Page({
     wxloginModule.wxlogin()
     this.setData({tabbarItemList: app.globalData.adminTabbarItem,
     tabIndex: 0})
+    /*
+    var md5String = md5("appId=wxd678efh567hg6787&nonceStr=5K8264ILTKCH16CQ2502SI8ZNMTM67VS&package=prepay_id=wx2017033010242291fcfe0db70013231072&signType=MD5&timeStamp=1490840662&key=qazwsxedcrfvtgbyhnujmikolp111111")
+    console.log(md5String)
+    */
   },
 
   /**
