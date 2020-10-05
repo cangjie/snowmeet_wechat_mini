2023b73d0c2f975cb72cce36bb2ad6886be04fb2
cangjie <cangjie@hotmail.com>
2020-10-05 17:35:00 +0800

    add face to face sheet

---
+++ pages/admin/equip_maintain/accept_equip/accept_equip.js
@@ -0,0 +1,68 @@
+// pages/admin/equip_maintain/accept_equip/accept_equip.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    skiBrandArray:['请选择...', 'APO', 'Fischer', 'Head', 'StockLi', 'Nordica', 'Atomic', '其它...'],
+    boardBrandArray:['请选择...', 'Nitro', 'Burton', 'Capita', 'APO', '其它...'],
+    brandIndex: 0
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
