55f166a05d46399c110ff19f19f93293d8e1befe
cangjie <cangjie@hotmail.com>
2020-10-12 19:56:45 +0800

    upload components with parameter

---
+++ components/uploader/multi-uploader.js
@@ -0,0 +1,70 @@
+// components/uploader/multi-uploader.js
+const app = getApp()
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    detail_id: {
+      type: String,
+      value: '0'
+    },
+    image_count: {
+      type: String,
+      value: '3'
+    }
+  },
+  /**
+   * Component initial data
+   */
+  data: {
+    files:[]
+  },
+  pageLifetimes: {
+    show: function() {
+      this.setData({
+        selectFile: this.selectFile.bind(this),
+        uploadFile: this.uploadFile.bind(this)
+      })
+    }
+  },
+  /**
+   * Component methods
+   */
+  methods: {
+    selectFile: function(files) {
+      console.log('files', files)
+      // 返回false可以阻止某次文件上传
+    },
+    uploadFile: function(files) {
+      console.log('upload files', files)
+      var uploadUrl = 'https://' + app.globalData.domainName + '/upload.aspx?sessionkey=' + encodeURIComponent(app.globalData.sessionKey)
+      // 文件上传的函数，返回一个promise
+      return new Promise((resolve, reject) => {
+        for(var i = 0; i < files.tempFilePaths.length; i++)
+        {
+          
+          wx.uploadFile({
+            filePath: files.tempFilePaths[i],
+            name: 'name',
+            url: uploadUrl,
+            success: (res)=>{
+              var filesData = this.data.files
+              //var filesData = [{url: 'http://mini.luqinwenda.com/upload/1596954732.jpg'}]
+              var uploadFilesData = res.data.split(',')
+              for(var i = 0; i < uploadFilesData.length; i++) {
+                filesData.push({url: 'https://' + app.globalData.domainName + uploadFilesData[i]})
+              }
+              this.setData({files: filesData})
+              console.log(res)
+            },
+            fail: (res)=>{
+              console.log(res)
+            }
+          })
+        }
+      })
+    }
+  },
+  
+})
