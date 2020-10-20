b69561980ceb63f4a5bf0bc8fda0a46fbd49adf3
cangjie <cangjie@hotmail.com>
2020-10-20 20:40:10 +0800

    picker

--- components/ski_board_base_info/ski_board_base_info.js
+++ components/ski_board_base_info/ski_board_base_info.js
@@ -1,4 +1,5 @@
 // components/ski_board_base_info/ski_board_base_info.js
+const app = getApp()
 Component({
   /**
    * Component properties
@@ -26,12 +27,24 @@ Component({
     }
   },
 
+  lifetimes: {
+    attached: function() {
+      var url = 'https://' + app.globalData.domainName + '/api/select_table.aspx?sessionkey=' + encodeURIComponent(app.globalData.sessionKey) + '&sql=' + encodeURIComponent('select * from ski_board_scale_list')
+      wx.request({
+        url: url,
+        success: (res) => {
+          this.setData({allSkiBoardScales: res.data})
+        }
+      })
+    }
+  },
   /**
    * Component initial data
    */
   data: {
     years: ['请选择', '20-21', '19-20', '18-19', '17-18', '16-17', '15-16', '14-15', '13-14', '12-13', '11-12', '10-11', '09-10'],
-    yearIndex: 0
+    yearIndex: 0,
+    
   },
 
   /**
@@ -40,9 +53,10 @@ Component({
   methods: {
     selectSkiBoardInfo: function(source) {
       var s = source
+    },
+    yearPickerChange: function(e) {
+      this.setData({yearIndex: e.detail.value})
     }
   },
-  bindPickerChange: function(source) {
-    var s = source
-  }
+  
 })
