7b290a117086fb165970cabf84ca668d04aa0a07
cangjie <cangjie@hotmail.com>
2020-11-17 13:51:34 +0800

    Revert "error"
    This reverts commit eee01742a4fd21cb26ad02d59819facf53ab38e0.

--- app.js
+++ app.js
@@ -47,9 +47,10 @@ App({
             //console.log(res.data)
             try
             {
+              
               this.globalData.sessionKey = res.data.session_key
-              this.globalData.role = res.data.role
               /*
+              this.globalData.role = res.data.role
               if (this.globalData.role == 'staff') {
                 wx.navigateTo({
                   url: '/pages/admin/equip_maintain/order_list/order_list_main',
@@ -66,7 +67,48 @@ App({
 
       }
     })
-   
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+              this.globalData.encryptedData = res.encryptedData
+              this.globalData.iv = res.iv
+              wx.request({
+                url: 'https://' + this.globalData.domainName + '/api/decode_encrypted_data.aspx',
+                data:{
+                  encdata: res.encryptedData,
+                  sessionkey: this.globalData.sessionKey,
+                  iv: res.iv
+                },
+                success: res => {
+                  if (res.data.encdata == this.globalData.encryptedData && res.data.sessionkey == this.globalData.sessionKey && res.data.iv == this.globalData.iv) {
+                    console.log('well')
+                  }
+                }
+              })
+              //console.log('注册成功123abc!@#','aes加密：',aes.aesEncrypt('注册成功123abc!@#'))
+              //console.log(aes.aesEncrypt('注册成功123abc!@#'),'aes解密：',aes.aesDecrypt(aes.aesEncrypt('注册成功123abc!@#')))
+            /*
+              var encStr = crypto.encrypted("福田区", this.globalData.sessionKey, res.iv)
+              console.log(encStr)//加密
+              var decStr = crypto.decrypted(res.encryptedData, this.globalData.sessionKey, res.iv)
+              console.log(decStr)//解密
+*/
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
   },
   globalData: {
     appId:'wxd1310896f2aa68bb',
@@ -102,6 +144,26 @@ App({
         "text": "上传测试",
         "iconPath": "images/home-2-line.png",
         "selectedIconPath": "images/home-2-line-hl.png",
+      }
+    ],
+    userTabBarItem: [
+      {
+        "pagePath": "/pages/index/index",
+        "text": "装备",
+        "iconPath": "images/zhihu-fill.png",
+        "selectedIconPath": "images/zhihu-fill-hl.png"
+      },
+      {
+        "pagePath": "/pages/skipass/skipass",
+        "text": "雪票",
+        "iconPath": "images/book-3-line.png",
+        "selectedIconPath": "images/book-3-line-hl.png"
+      },
+      {
+        "pagePath": "/pages/mine/mine",
+        "text": "我的",
+        "iconPath": "images/home-2-line.png",
+        "selectedIconPath": "images/home-2-line-hl.png"
       }
     ]
   }
