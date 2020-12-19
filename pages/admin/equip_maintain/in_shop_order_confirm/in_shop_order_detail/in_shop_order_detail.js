3cc73fccd89fac6b6ea7e4d39a88cf1a004532f7
杰 苍 <cangjie@hotmail.com>
2020-12-19 18:34:16 +0800

    simple equip info

--- pages/admin/equip_maintain/in_shop_order_confirm/in_shop_order_detail/in_shop_order_detail.js
+++ pages/admin/equip_maintain/in_shop_order_confirm/in_shop_order_detail/in_shop_order_detail.js
@@ -19,7 +19,107 @@ Page({
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
+  onLoad: function(options){
+    var id = options.scene
+    var getBrandListPromise = new Promise(function(resolve){
+      var brandListUrl = 'https://' + app.globalData.domainName + '/api/brand_list_get.aspx'
+      wx.request({
+        url: brandListUrl,
+        success: (res) => {
+          var listArray = res.data.brand_list
+          var skiList = []
+          skiList.push('请选择...')
+          var boardList = []
+          boardList.push('请选择...')
+          for(var i = 0; i < listArray.length; i++){
+            if (listArray[i].brand_type == '双板') {
+              skiList.push(listArray[i].brand_name+ (listArray[i].chinese_name.trim() != ''?'/':'')+listArray[i].chinese_name)
+            }
+            else{
+              boardList.push(listArray[i].brand_name+ (listArray[i].chinese_name.trim() != ''?'/':'')+listArray[i].chinese_name)
+            }
+            
+          }
+          skiList.push('未知')
+          boardList.push('未知')
+          that.setData({skiBrandList: skiList, boardBrandList: boardList})
+          resolve({})
+        }
+      })
+    })
+    var that = this
+    getBrandListPromise.then(function(resolve){
+      app.loginPromise.then(function(resolve){
+        if (app.globalData.sessionKey != '') {
+          var getUserFilledInfoPromise = new Promise(function(resolve) {
+            var url = 'https://' + app.globalData.domainName + '/api/maintain_task_request_in_shop_get.aspx?sessionkey=' + encodeURIComponent(app.globalData.sessionKey) + '&id=' + id
+            wx.request({
+              url: url,
+              success: (res) => {
+                if (res.data.status == 0) {
+                  var maintainInShopRequest = res.data.maintain_in_shop_request
+                  var pickDate = new Date(maintainInShopRequest.pick_date)
+                  maintainInShopRequest.pick_date = pickDate.getFullYear().toString() + '-' + (1 + pickDate.getMonth()).toString() + '-' + pickDate.getDate().toString()
+                  that.setData({maintain_in_shop_request: res.data.maintain_in_shop_request})
+                  resolve(res.data.maintain_in_shop_request)
+                } 
+              }
+            })
+          })
+          getUserFilledInfoPromise.then(function(resolve){
+            var getUserInfoPromise = new Promise(function(resolve){
+              var url = 'https://' + app.globalData.domainName + '/api/mini_user_get.aspx?sessionkey=' + encodeURIComponent(app.globalData.sessionKey) + '&openid=' + encodeURIComponent(that.data.maintain_in_shop_request.open_id)
+              wx.request({
+                url: url,
+                success: (res) => {
+                  if (res.data.status == 0 && res.data.count > 0){
+                    resolve(res.data.mini_users[0])
+                  }
+                }
+              })
+            })
+            getUserInfoPromise.then(function(resolve){
+              var confirmedInfo = {}
+              confirmedInfo.request_id = that.data.maintain_in_shop_request.id
+              confirmedInfo.cell_number = resolve.cell_number
+              confirmedInfo.real_name = resolve.real_name
+              confirmedInfo.gender = resolve.gender
+              var equipInfo = {}
+              equipInfo.type = that.data.maintain_in_shop_request.equip_type
+              equipInfo.brand = that.data.maintain_in_shop_request.brand
+              equipInfo.serial = ''
+              equipInfo.scale = that.data.maintain_in_shop_request.scale
+              equipInfo.year = ''
+              confirmedInfo.equipInfo = equipInfo
+              confirmedInfo.edge = that.data.maintain_in_shop_request.edge
+              confirmedInfo.degree = that.data.maintain_in_shop_request.degree
+              confirmedInfo.candle = that.data.maintain_in_shop_request.candle
+              confirmedInfo.repair_more = that.data.maintain_in_shop_request.repair_more
+              confirmedInfo.shop = that.data.maintain_in_shop_request.shop
+              confirmedInfo.degree = '89'
+              var displayedBrandList = that.data.skiBrandList
+              if (equipInfo.type=='双板') {
+                displayedBrandList = that.data.skiBrandList
+              }
+              else {
+                displayedBrandList = that.data.boardBrandList
+              }
+              var brandSelectIndex = 0
+              for(var i = 0; i < displayedBrandList.length; i++) {
+                if (displayedBrandList[i].trim() == equipInfo.brand) {
+                  brandSelectIndex = i
+                  break
+                }
+              }
+              that.setData({userInfo: resolve, confirmedInfo: confirmedInfo, displayedBrandList: displayedBrandList, brandSelectIndex: brandSelectIndex})
+              that.viewSummary('view')
+            })
+          })
+        }
+      })
+    })
+  },
+  onLoad1: function (options) {
     var optionArr = options.scene.split('-')
     var id = optionArr[0]
     var equipType = '双板'
@@ -222,6 +322,25 @@ Page({
     this.setData({confirmedInfo: confirmedInfo})
     this.viewSummary('view')
   },
+  selectType: function(e) {
+    if (e.detail.value=='单板') {
+      this.setData({brandSelectIndex: 0, displayedBrandList: this.data.boardBrandList})
+    }
+    else {
+      this.setData({brandSelectIndex: 0, displayedBrandList: this.data.skiBrandList})
+    }
+  },
+  changeScale: function(e) {
+    var confirmedInfo = this.data.confirmedInfo
+    confirmedInfo.equipInfo.scale = e.detail.value
+    this.setData({confirmedInfo: confirmedInfo})
+  },
+  selectBrand: function(e){
+    var confirmedInfo = this.data.confirmedInfo
+    confirmedInfo.equipInfo.brand = this.data.displayedBrandList[e.detail.value]
+    this.setData({brandSelectIndex: e.detail.value, confirmedInfo: confirmedInfo})
+    this.viewSummary('view')
+  },
   submit: function(e) {
     this.viewSummary('placeorder')
   },
@@ -253,7 +372,7 @@ Page({
           catch(err) {
 
           }
-          if (type == '' || type == undefined || brand == '' || brand == undefined ||  serial == '' || serial == undefined || year == '' || year == undefined || scale == '' || scale == undefined) {
+          if (type == '' || type == undefined || brand == '' || brand == undefined) {
             btnText = '请选择装备信息'
           }
           else {
--- pages/maintain/in_shop_request/in_shop_request.js
+++ pages/maintain/in_shop_request/in_shop_request.js
@@ -21,7 +21,7 @@ Page({
     startDate: '',
     endDate:'',
     id: '0',
-    
+    brandSelectIndex: 0
   },
   
   /**
@@ -35,11 +35,38 @@ Page({
     pickDate.setDate(now.getDate() + 1)
     
     this.setData({pickDate: pickDate.getFullYear().toString() + '-' + (pickDate.getMonth() + 1).toString() + '-' + pickDate.getDate().toString() , startDate: now.toDateString(), endDate: endDate.toDateString(), shop: options.shop})
+
+    var brandListUrl = 'https://' + app.globalData.domainName + '/api/brand_list_get.aspx'
+    wx.request({
+      url: brandListUrl,
+      success: (res) => {
+        var listArray = res.data.brand_list
+        var skiList = []
+        skiList.push('请选择...')
+        var boardList = []
+        boardList.push('请选择...')
+        for(var i = 0; i < listArray.length; i++){
+          if (listArray[i].brand_type == '双板') {
+            skiList.push(listArray[i].brand_name+ (listArray[i].chinese_name.trim() != ''?'/':'')+listArray[i].chinese_name)
+          }
+          else{
+            boardList.push(listArray[i].brand_name+ (listArray[i].chinese_name.trim() != ''?'/':'')+listArray[i].chinese_name)
+          }
+          
+        }
+        skiList.push('未知')
+        boardList.push('未知')
+        this.setData({skiBrandList: skiList, boardBrandList: boardList, displayedBrandList: skiList})
+      }
+    })
+
+
     if (app.globalData.sessionKey == null || app.globalData.sessionKey == '') {
       var that = this
       app.loginPromise.then(function(resolve) {
         that.setData({sessionKey: app.globalData.sessionKey})
       })
+
     }
   },
 
@@ -103,7 +130,8 @@ Page({
   },
   submit: function(e) {
     if (this.checkValid()) {
-      var url = 'https://' + app.globalData.domainName + '/api/maintain_task_request_in_shop_create.aspx?sessionkey=' + encodeURIComponent(this.data.sessionKey.trim()) + '&shop=' + encodeURIComponent(this.data.shop) + '&type=' + encodeURIComponent(this.data.type) + '&brand=' + encodeURIComponent(this.data.brand) + '&scale=' + encodeURIComponent(this.data.scale) + '&edge=' + (this.data.edge? '1' : '0') + '&candle=' + (this.data.candle? '1' : '0') + '&repair=' + (this.data.repair? '1':'0') + '&pickdate=' + encodeURIComponent(this.data.pickDate)
+      var brand = this.data.displayedBrandList[this.data.brandSelectIndex]
+      var url = 'https://' + app.globalData.domainName + '/api/maintain_task_request_in_shop_create.aspx?sessionkey=' + encodeURIComponent(this.data.sessionKey.trim()) + '&shop=' + encodeURIComponent(this.data.shop) + '&type=' + encodeURIComponent(this.data.type) + '&brand=' + encodeURIComponent(brand) + '&scale=' + encodeURIComponent(this.data.scale) + '&edge=' + (this.data.edge? '1' : '0') + '&candle=' + (this.data.candle? '1' : '0') + '&repair=' + (this.data.repair? '1':'0') + '&pickdate=' + encodeURIComponent(this.data.pickDate)
       wx.request({
         url: url,
         success: (res) => {
@@ -113,7 +141,7 @@ Page({
             if (this.data.type.trim() == '单板') {
               type = 'board'
             }
-            var wxaCodeUrl = 'https://' + app.globalData.domainName + '/get_wxacode_unlimit.aspx?page=' + encodeURIComponent('pages/admin/equip_maintain/in_shop_order_confirm/in_shop_order_detail/in_shop_order_detail') + '&scene=' + res.data.id + '-' + type
+            var wxaCodeUrl = 'https://' + app.globalData.domainName + '/get_wxacode_unlimit.aspx?page=' + encodeURIComponent('pages/admin/equip_maintain/in_shop_order_confirm/in_shop_order_detail/in_shop_order_detail') + '&scene=' + res.data.id
             this.setData({wxaCodeUrl: wxaCodeUrl, id: res.data.id})
             var requestId = res.data.id
             //var that = this
@@ -150,7 +178,7 @@ Page({
     }
   },
   checkValid: function() {
-    if (this.data.brand.trim() != '') {
+    if (this.data.brandSelectIndex > 0) {
       if (this.data.edge || this.data.candle || this.data.repair) {
         this.setData({buttonDisable: false, buttonText: " 提 交 "})
         return true
@@ -165,7 +193,12 @@ Page({
     }
   },
   selectType: function(e) {
-    this.setData({type: e.detail.value.trim()})
+    var brandList = this.data.skiBrandList
+    if (e.detail.value == '单板') {
+      brandList = this.data.boardBrandList
+    }
+
+    this.setData({type: e.detail.value.trim(), displayedBrandList: brandList, brandSelectIndex: 0})
     this.checkValid()
   },
   inputBrand: function(e) {
@@ -187,6 +220,10 @@ Page({
       }
     }
     this.setData({edge: edge, candle:candle})
+    this.checkValid()
+  },
+  selectBrand: function(e){
+    this.setData({brandSelectIndex: e.detail.value})
     this.checkValid()
   }
 })
