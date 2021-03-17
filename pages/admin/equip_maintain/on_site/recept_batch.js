2c78df954b78edfd9e1419fc2f48ab2358080ab7
cangjie <cangjie@hotmail.com>
2021-03-17 20:17:39 +0800

    batch

---
+++ pages/admin/equip_maintain/on_site/recept_batch.js
@@ -0,0 +1,73 @@
+// pages/admin/equip_maintain/on_site/recept_batch.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    wx.showModal({
+      title: '是否添加添加下一单？',
+      confirmText: '新增',
+      cancelText: '结算',
+      success: (res) => {
+        
+      }
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
