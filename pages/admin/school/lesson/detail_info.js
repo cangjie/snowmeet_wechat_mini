25bd065059504bdf5754ed012dc95fa6c5138abc
cangjie <cangjie@hotmail.com>
2021-08-02 22:33:00 +0800

    finish upload video control

--- pages/admin/school/lesson/detail_info.js
+++ pages/admin/school/lesson/detail_info.js
@@ -1,18 +1,22 @@
 // pages/admin/school/lesson/detail_info.js
+const app = getApp()
 Page({
 
   /**
    * Page initial data
    */
   data: {
-    uploadedFiles: ''
+    
+    videoThumbs: []
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    //var videoThumbs = this.data.videoThumbs
+    //videoThumbs.push({url: 'https://mini.snowmeet.top/upload/20210802/1627912001.jpg'})
+    //this.setData({videoThumbs: videoThumbs})
   },
 
   /**
@@ -26,7 +30,7 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow: function () {
-
+    this.setData({uploadVideo: this.uploadVideo.bind(this)})
   },
 
   /**
@@ -63,6 +67,7 @@ Page({
   onShareAppMessage: function () {
 
   },
+  /*
   uploaded: function(e) {
     var files = e.detail.files
     var uploadedFiles = ''
@@ -72,5 +77,50 @@ Page({
       }
     }
     this.setData({uploadedFiles: uploadedFiles})
+  },
+  */
+  uploadVideo: function(files) {
+    var uploadUrl = 'https://' + app.globalData.domainName + '/upload_video.aspx?sessionkey=' + encodeURIComponent(app.globalData.sessionKey)
+    return new Promise((resolve, reject) => {
+      wx.uploadFile({
+        filePath: files.thumbTempFilePath,
+        name: 'name',
+        url: uploadUrl,
+        success: (res) => {
+          var timeStampArr = res.data.trim().split('/')
+          var timeStamp = timeStampArr[timeStampArr.length - 1].trim()
+          timeStamp = timeStamp.split('.')[0].trim()
+          uploadUrl = uploadUrl + '&timestamp=' + timeStamp
+          wx.uploadFile({
+            filePath: files.tempFilePath,
+            name: 'name',
+            url: uploadUrl,
+            success: (res) => {
+              resolve({"urls": [res.data.trim()]})
+            }
+          })
+        }
+      })
+    })
+  },
+  deleteVideo: function(res) {
+    var videoThumbs = this.data.videoThumbs
+    var delIndex = res.detail.index
+    var newVideoThumbs = []
+    for(var i = 0; i < videoThumbs.length; i++) {
+      if (i != delIndex) {
+        newVideoThumbs.push(videoThumbs[i])
+      }
+    }
+    this.setData({videoThumbs: newVideoThumbs})
+  },
+  uploadVideoSuccess: function(res) {
+    var r = res.detail.urls
+    var newVideoThumb = 'https://' + app.globalData.domainName.trim() + r[0]
+    newVideoThumb = newVideoThumb.toLowerCase().replace('.mp4', '.jpg')
+    var videoThumbs = this.data.videoThumbs
+    videoThumbs.push({url: newVideoThumb})
+    this.setData({videoThumbs: videoThumbs})
   }
+  
 })
