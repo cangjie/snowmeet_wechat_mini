d4467481638a0d728bb50e046d8a2260464cf9cc
cangjie <cangjie@hotmail.com>
2022-02-11 10:52:08 +0800

    add new page and component with css

---
+++ components/ski_board_brand_selector/ski_board_brand_selector.js
@@ -0,0 +1,23 @@
+// components/ski_board_brand_selector/ski_board_brand_selector.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
