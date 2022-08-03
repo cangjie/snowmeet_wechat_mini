e8ccdf509ca3f007284a4278d55a38232df0d955
cangjie <cangjie@hotmail.com>
2022-08-03 23:00:03 +0800

    mi7 order components

---
+++ components/mi7_order/mi7_order.js
@@ -0,0 +1,23 @@
+// components/mi7_order/mi7_order.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
