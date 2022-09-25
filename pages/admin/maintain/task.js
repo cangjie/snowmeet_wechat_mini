ac256c9956508aa8a21cba7e33fe3a06dab10b30
cangjie <cangjie@hotmail.com>
2022-09-25 17:42:10 +0800

    interface mod

--- pages/admin/maintain/task.js
+++ pages/admin/maintain/task.js
@@ -7,13 +7,18 @@ Page({
    * Page initial data
    */
   data: {
-
+    yearList: ['未知', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023'],
+    yearListSelectedIndex: 0,
+    serialDiff: false,
+    showGallery: false,
+    galleryIndex: -1
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad(options) {
+    var that = this
     app.loginPromiseNew.then(function(resolve){
       var getInfoUrl = 'https://' + app.globalData.domainName + '/core/MaintainLive/GetTask/' + options.id + '?sessionKey=' + encodeURIComponent(app.globalData.sessionKey)
       wx.request({
@@ -21,9 +26,40 @@ Page({
         method: 'GET',
         success:(res)=>{
           console.log('task info', res)
+          var task = res.data
+          task.images = []
+          if (task.confirmed_images != ''){
+            task.images = task.confirmed_images.split(',')
+          }
+          var getSerialUrl = 'https://' + app.globalData.domainName + '/core/MaintainLive/GetSerials?brand=' + encodeURIComponent(task.confirmed_brand)
+          wx.request({
+            url: getSerialUrl,
+            method: 'GET',
+            success:(res)=>{
+              var serialList = res.data
+              serialList.push('未知')
+              var serialSelectedIndex = serialList.length - 1
+              that.setData({task: task, serialList: serialList, serialSelectedIndex: serialSelectedIndex})
+            }
+          })
+          
         }
       })
     })
+  },
+  showGallery(e){
+    var id = e.currentTarget.id
+    var that = this
+    that.setData({showGallery: true, galleryIndex: parseInt(id)})
+  },
+  setSerialDiff(e){
+    var that = this
+    if (e.detail.value == 'serial_diff'){
+      that.setData({serialDiff: true})
+    }
+    else{
+      that.setData({serialDiff: false})
+    }
   },
 
   /**
