60393bad6de14e212675fcdcdf1d5a648ddc3be5
cangjie <cangjie@hotmail.com>
2023-02-01 15:26:15 +0800

    report

--- pages/admin/rent/rent_report.js
+++ pages/admin/rent/rent_report.js
@@ -20,7 +20,8 @@ Page({
     currentBusinessRental: 0,
     currentBusinessRentalSettled:0,
     currentBusinessRentalUnSettled:0,
-    settledBeforeRental: 0
+    settledBeforeRental: 0,
+    scene: 0
   },
 
   changeDate(e){
@@ -44,34 +45,52 @@ Page({
         console.log('unsettled before', res)
 
         var orders = res.data.orders
+
+
+
         var currentBusinessRental = that.data.currentBusinessRental
         var currentBusinessRentalSettled = that.data.currentBusinessRentalSettled
         var currentBusinessRentalUnSettled = that.data.currentBusinessRentalUnSettled
         for(var i = 0; orders != null && i < orders.length; i++){
+          var createDate = new Date(orders[i].create_date)
+          orders[i].create_date_str = util.formatDate(createDate)
+          orders[i].create_time_str = util.formatTimeStr(createDate)
+          orders[i].deposit_final_str = util.showAmount(orders[i].deposit_final)
+          var orderRental = 0
           var rentalList = orders[i].rentalDetails
           for(var j = 0; rentalList != null && j < rentalList.length; j++){
             var rental = rentalList[j]
             var currentDate = new Date(that.data.currentDate.toString()+'T00:00:00')
             var rentalDate = new Date(rental.date)
+            var rentalAmount = parseFloat(rental.rental)
+            
+            
             if (currentDate.getFullYear() == rentalDate.getFullYear()
               && currentDate.getMonth() == rentalDate.getMonth()
               && currentDate.getDate() == rentalDate.getDate()){
-              var rentalAmount = parseFloat(rental.rental)
+              
               if (!isNaN(rentalAmount)){
                 currentBusinessRental = currentBusinessRental + rentalAmount
                 if (rental.type != ''){
                   currentBusinessRentalSettled = currentBusinessRentalSettled + rentalAmount
+                  
                 }
                 else{
                   currentBusinessRentalUnSettled += rentalAmount
+                  
                 }
               }
               
             }
+            else if (currentDate.valueOf() >= rentalDate.valueOf()){
+              orderRental += rentalAmount
+            }
           }
+          orders[i].orderRentalStr = util.showAmount(orderRental)
         }
+
         that.setData({unRefundDeposit: res.data.unRefundDeposit, unRefundDepositStr: util.showAmount(res.data.unRefundDeposit), unSettledReantal: res.data.unSettledRental, unSettledRentalStr: util.showAmount(res.data.unSettledRental), currentBusinessRental: currentBusinessRental,
-          currentBusinessRentalStr: util.showAmount(currentBusinessRental), currentBusinessRentalSettled: currentBusinessRentalSettled, currentBusinessRentalSettledStr: util.showAmount(currentBusinessRentalSettled), currentBusinessRentalUnSettled: currentBusinessRentalUnSettled, currentBusinessRentalUnSettledStr: util.showAmount(currentBusinessRentalUnSettled)})
+          currentBusinessRentalStr: util.showAmount(currentBusinessRental), currentBusinessRentalSettled: currentBusinessRentalSettled, currentBusinessRentalSettledStr: util.showAmount(currentBusinessRentalSettled), currentBusinessRentalUnSettled: currentBusinessRentalUnSettled, currentBusinessRentalUnSettledStr: util.showAmount(currentBusinessRentalUnSettled), UnSettledOrderBeforeSet: orders})
         wx.request({
           url: getCurrentDayPlacedUrl,
           method: 'GET',
@@ -109,13 +128,13 @@ Page({
 
 
 
-            that.setData({currentTotalDeposit: res.data.totalDeposit, currentTotalDepositStr: util.showAmount(res.data.totalDeposit), currentBusinessRental: currentBusinessRental, currentBusinessRentalStr: util.showAmount(currentBusinessRental), currentBusinessRentalSettled: currentBusinessRentalSettled, currentBusinessRentalSettledStr: util.showAmount(currentBusinessRentalSettled), currentBusinessRentalSettled: currentBusinessRentalSettled, currentBusinessRentalSettledStr: util.showAmount(currentBusinessRentalSettled), currentBusinessRentalUnSettled: currentBusinessRentalUnSettled, currentBusinessRentalUnSettledStr: util.showAmount(currentBusinessRentalUnSettled)})
+            that.setData({currentTotalDeposit: res.data.totalDeposit, currentTotalDepositStr: util.showAmount(res.data.totalDeposit), currentBusinessRental: currentBusinessRental, currentBusinessRentalStr: util.showAmount(currentBusinessRental), currentBusinessRentalSettled: currentBusinessRentalSettled, currentBusinessRentalSettledStr: util.showAmount(currentBusinessRentalSettled), currentBusinessRentalSettled: currentBusinessRentalSettled, currentBusinessRentalSettledStr: util.showAmount(currentBusinessRentalSettled), currentBusinessRentalUnSettled: currentBusinessRentalUnSettled, currentBusinessRentalUnSettledStr: util.showAmount(currentBusinessRentalUnSettled), CurrentDayPlacedSet: res.data})
             wx.request({
               url: getCurrentSameDaySettledUrl,
               method:'GET',
               success:(res)=>{
                 console.log('today settled', res)
-                that.setData({sameDaySettledRental: res.data.totalRental, sameDaySettledRentalStr: util.showAmount(res.data.totalRental), sameDayRefundDeposit: res.data.totalDeposit, sameDayRefundDepositStr: util.showAmount(res.data.totalDeposit)})
+                that.setData({sameDaySettledRental: res.data.totalRental, sameDaySettledRentalStr: util.showAmount(res.data.totalRental), sameDayRefundDeposit: res.data.totalDeposit, sameDayRefundDepositStr: util.showAmount(res.data.totalDeposit), CurrentSameDaySettledSet: res.data})
                 wx.request({
                   url: getCurrentDaySettledPlacedBeforeUrl,
                   method:'GET',
@@ -126,15 +145,30 @@ Page({
                     var settledBeforeRental = that.data.settledBeforeRental
                     var orders = res.data.orders
 
+                    
+
+
                     for(var i = 0; orders != null && i < orders.length; i++){
                       var details = orders[i].details
+                      var orderRental = 0
+
+                      var createDate = new Date(orders[i].create_date)
+
+                      orders[i].create_date_str = util.formatDate(createDate)
+                      orders[i].create_time_str = util.formatTimeStr(createDate)
+                      orders[i].deposit_final_str = util.showAmount(orders[i].deposit_final)
+
+
                       for(var j = 0; details != null && j < details.length; j++){
                         var detail = details[j]
+                        
                         var amount = parseFloat(details[j].real_rental)
                         if (!isNaN(amount)){
                           settledBeforeRental += amount
+                          orderRental += amount
                         }
                       }
+                      orders[i].orderRentalStr = util.showAmount(orderRental)
                     }
 
 
@@ -142,7 +176,7 @@ Page({
                       currentDayRefundPlacedBeforeStr: util.showAmount(res.data.totalDeposit),
                       currentDateUnRefundDeposit: currentDateUnRefundDeposit, 
                       currentDateUnRefundDepositStr: util.showAmount(currentDateUnRefundDeposit), settledBeforeRental: settledBeforeRental,
-                      settledBeforeRentalStr: util.showAmount(settledBeforeRental)})
+                      settledBeforeRentalStr: util.showAmount(settledBeforeRental), CurrentDaySettledPlacedBeforeSet: orders})
                   }
                 })
               }
@@ -152,6 +186,16 @@ Page({
       }
     })
 
+  },
+
+  goBack(){
+    var that = this
+    that.setData({scene: 0})
+  },
+  showDetail(e){
+    var id = e.currentTarget.id
+    var that = this
+    that.setData({scene: parseInt(id)})
   },
 
   /**
