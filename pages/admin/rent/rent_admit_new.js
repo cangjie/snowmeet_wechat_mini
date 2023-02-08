865d7ae54364a5d282570b8790681d46e6252726
cangjie <cangjie@hotmail.com>
2023-02-08 14:53:34 +0800

    add a new page

---
+++ pages/admin/rent/rent_admit_new.js
@@ -0,0 +1,72 @@
+// pages/admin/rent/rent_admit_new.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  goBack(){
+    wx.redirectTo({
+      url: 'rent_admit',
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage() {
+
+  }
+})
