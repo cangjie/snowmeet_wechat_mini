b208cb3200a7850df8426acf0bf7d23b154fcfa7
cangjie <cangjie@hotmail.com>
2023-02-08 19:45:45 +0800

    new rent

--- pages/admin/rent/rent_admit_new.js
+++ pages/admin/rent/rent_admit_new.js
@@ -1,11 +1,14 @@
 // pages/admin/rent/rent_admit_new.js
+const app = getApp()
+const util = require('../../../utils/util.js')
 Page({
 
   /**
    * Page initial data
    */
   data: {
-
+    scene: 0,
+    classSelectIndex: 0
   },
 
   goBack(){
@@ -20,7 +23,24 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad(options) {
-
+    var that = this
+    app.loginPromiseNew.then(function (resolve){
+      var getClassUrl = 'https://' + app.globalData.domainName + '/core/Rent/GetRentClass'
+      wx.request({
+        url: getClassUrl,
+        method: 'GET',
+        success:(res)=>{
+          if (res.statusCode != 200){
+            return
+          }
+          var classList = ['请选择。。。']
+          for(var i = 0; i < res.data.length; i++){
+            classList.push(res.data[i])
+          }
+          that.setData({classList: classList})
+        }
+      })
+    })
   },
 
   /**
