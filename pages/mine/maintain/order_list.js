85d4cb1c4688ac886438ff9d5d5c849404b0baa6
cangjie <cangjie@hotmail.com>
2024-01-25 22:57:03 +0800

    get my own maintain list

--- pages/mine/maintain/order_list.js
+++ pages/mine/maintain/order_list.js
@@ -15,29 +15,44 @@ Page({
       url: 'order_detail?id=' + e.currentTarget.id,
     })
   },
+  getData(){
+    var that = this
+    var getOrdersUrl = 'https://' + app.globalData.domainName + '/core/MaintainLive/GetMyMaintainOrders?sessionKey=' + encodeURIComponent(app.globalData.sessionKey)
+    wx.request({
+      url: getOrdersUrl,
+      method: 'GET',
+      success:(res)=>{
+        console.log('get my maintain orders', res)
+        if (res.statusCode != 200){
+          return
+        }
+        var orders = res.data
+        var items = []
+        for(var i = 0; i < orders.length && items.length < 5; i++){
+          for(var j = 0; j < orders[i].items.length; j++){
+            var item = orders[i].items[j]
+            //item.order = orders[i]
+            item.order_id = orders[i].orderId
+            item.final_price = orders[i].final_price
+            item.image = item.confirmed_images.split(',')[0]
+            if (item.task_flow_num != undefined && item.task_flow_num != null && item.task_flow_num != ''){
+              items.push(item)
+            }
+            
+          }
+        }
+        that.setData({items: items})
+      }
+    })
+
+  },
   /**
    * Lifecycle function--Called when page load
    */
   onLoad(options) {
     var that = this
     app.loginPromiseNew.then(function (resolve) {
-      var getOrdersUrl = 'https://' + app.globalData.domainName + '/core/MaintainLive/GetMyMaintainOrders?sessionKey=' + app.globalData.sessionKey
-      wx.request({
-        url: getOrdersUrl,
-        method: 'GET',
-        success:(res)=>{
-          if (res.statusCode == 200){
-            var orders = res.data
-            for(var i = 0; i < orders.length; i++){
-              var order = orders[i]
-              order.date = util.formatDate(new Date(order.order.create_date))
-              order.time = util.formatTimeStr(new Date(order.order.create_date))
-            }
-            that.setData({orders: orders})
-          }
-          
-        }
-      })
+      that.getData()
     })
   },
 
