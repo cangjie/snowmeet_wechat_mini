5c58b0f988273b708ab6a370c42e88e083596b1a
cangjie <cangjie@hotmail.com>
2024-01-30 20:52:50 +0800

    scan to return

--- pages/admin/maintain/return_entry.js
+++ pages/admin/maintain/return_entry.js
@@ -1,4 +1,6 @@
 // pages/admin/maintain/return_entry.js
+const app = getApp()
+const util = require('../../../utils/util.js')
 Page({
 
   /**
@@ -7,12 +9,100 @@ Page({
   data: {
 
   },
+  checkScan(){
+    var that = this
+    var checkScanUrl = 'https://' + app.globalData.domainName + '/core/ShopSaleInteract/GetScanInfo/' + that.data.interviewId + '?sessionKey=' + encodeURIComponent(app.globalData.sessionKey)
+    var scanTimes = that.data.scanTimes
+    if (scanTimes == undefined){
+      that.setData({scanTimes: 1})
+    }
+    else{
+      that.setData({scanTimes: scanTimes++})
+    }
+    if (scanTimes > 900){
+      clearInterval(that.data.intervalId)
+    }
+    wx.request({
+      url: checkScanUrl,
+      success:(res)=>{
+        console.log('check scan', res)
+        if (res.statusCode != 200 && res.statusCode != 404){
+          clearInterval(that.data.intervalId)
+        }
+        else if (res.statusCode == 200){
+          
+          //clearInterval(that.data.interVal)
+          var scan = res.data
+          var needJump = false
+          if (scan.scan ==1){
+            var word = '顾客已扫码。'
+            if (scan.miniAppUser == null || scan.miniAppUser.cell_number == ''){
+              word = '顾客不是会员。'
+            }
+            else {
+              word = '顾客是会员。'
+              clearInterval(that.data.intervalId)
+              needJump = true
+            }
+            wx.showToast({
+              title: word,
+              duration: 2000
+            })
+            if (needJump){
+              //clearInterval(that.data.intervalId)
+              var jumpUrl = 'user_task_list?openId=' + res.data.miniAppUser.open_id
+              wx.redirectTo({
+                url: jumpUrl,
+              })
+            }
+          }
+        }
+      },
+      fail:(res)=>{
+        clearInterval(that.data.intervalId)
+      }
+    })
+  },
+  getInterviewId(){
+    var that = this
+    var getScanIdUrl = 'https://' + app.globalData.domainName + '/core/ShopSaleInteract/GetInterviewId?sessionKey=' + encodeURIComponent(app.globalData.sessionKey)
+    wx.request({
+      url: getScanIdUrl,
+      method: 'GET',
+      success:(res)=>{
+        if (res.statusCode != 200){
+          return
+        }
+        var interviewId = res.data
+        if (interviewId == 0){
+          return
+        }
+        that.setData({interviewId: interviewId})
+        var scene = 'recept_maintain_id_' + interviewId
+        var getQRUrl = 'https://wxoa.snowmeet.top/api/OfficialAccountApi/GetOAQRCodeUrl?content=' + scene
+        wx.request({
+          url: getQRUrl,
+          method: 'GET',
+          success:(res)=>{
+            console.log('get qrcode', res)
+            that.setData({qrcodeUrl: res.data})
+            var intervalId = setInterval(that.checkScan, 1000)
+            that.setData({intervalId: intervalId})
+          }
+        })
+        
+      }
+    })
+  },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad(options) {
-
+    var that = this
+    app.loginPromiseNew.then(function(resolve){
+      that.getInterviewId()
+    })
   },
 
   /**
---
+++ pages/admin/maintain/user_task_list.js
@@ -0,0 +1,123 @@
+// pages/admin/maintain/user_task_list.js
+const app = getApp()
+const util = require('../../../utils/util.js')
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+  getData(){
+    var that = this
+    var getTaskUrl = 'https://' + app.globalData.domainName + '/core/MaintainLive/GetTasks?start=' + encodeURIComponent('2020-01-01') + '&end=' + encodeURIComponent('2099-12-31') + '&sessionKey=' + encodeURIComponent(app.globalData.sessionKey) + ((that.data.shop == '')? '' : '&shop=&openId=' + encodeURIComponent(that.data.openId))
+    var orderList = []
+    wx.request({
+      url: getTaskUrl,
+      method: 'GET',
+      success:(res)=>{
+        console.log('get tasks', res)
+        var tasks = res.data
+        for(var i = 0; i < tasks.length; i++){
+          var task = tasks[i]
+          task.memo = ''
+          task.date = util.formatDate(new Date(task.create_date))
+          if (task.confirmed_equip_type == '双板' && task.confirmed_serial == ''){
+            task.memo = '信息不全'
+          }
+          if (task.order != undefined && task.order != null 
+            && task.order.final_price != undefined && task.order.final_price != null){
+              task.orderPriceStr = util.showAmount(task.order.final_price)
+          }
+          else {
+            task.orderPriceStr = '---'
+          }
+          if (task.order_id > 0){
+            var exists = false
+            for(var j = 0; j < orderList.length; j++){
+              if (orderList[j].id == task.order_id){
+                exists = true
+                break
+              }
+            }
+            if (!exists ){
+              orderList.push(task.order)
+            }
+          }
+        }
+        var totalAmount = 0
+        for(var i = 0; i < orderList.length; i++){
+          totalAmount += orderList[i].final_price
+        }
+        var totalAmountStr = util.showAmount(totalAmount)
+
+        that.setData({tasks: tasks, orderList: orderList, totalAmount: totalAmount, totalAmountStr: totalAmountStr})
+      }
+    })
+  },
+  gotoDetail(e){
+    wx.navigateTo({
+      url: 'task?id=' + e.currentTarget.id,
+    })
+  },
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad(options) {
+    var that = this
+    that.setData({openId: options.openId})
+    app.loginPromiseNew.then(function(reolve){
+      that.getData()
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage() {
+
+  }
+})
