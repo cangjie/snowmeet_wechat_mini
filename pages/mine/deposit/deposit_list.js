5c237e38e5f31d8c94a1033bc0518184f0e8b7bb
cangjie <cangjie@hotmail.com>
2025-02-20 21:23:22 +0800

    my deposits

---
+++ pages/mine/deposit/deposit_list.js
@@ -0,0 +1,86 @@
+// pages/mine/deposit/deposit_list.js
+const util = require('../../../utils/util')
+const app = getApp()
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+    var that = this
+    app.loginPromiseNew.then(function(resolve){
+      that.getData()
+    })
+  },
+
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage() {
+
+  },
+  getData(){
+    var that = this
+    var url = 'https://' + app.globalData.domainName + '/core/Deposit/GetMyAccounts?type=' + encodeURIComponent('服务储值') + '&subType=&sessionKey=' + encodeURIComponent(app.globalData.sessionKey) + '&sessionType=' + encodeURIComponent('wechat_mini_openid')
+    wx.request({
+      url: url,
+      method: 'GET',
+      success:(res)=>{
+        if (res.statusCode != 200){
+          return
+        }
+        console.log('accounts', res)
+      }
+    }) 
+  }
+})
