2f4629a4d274c2bb02e732e667b4b5ac6a2a4396
cangjie <cangjie@hotmail.com>
2025-08-31 15:29:28 +0800

    rental detail template

---
+++ pages/template/rent/rent_detail.js
@@ -0,0 +1,70 @@
+// pages/template/rent/rent_detail.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    activeNames: ['info', 'payments', 'rentals']
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage() {
+
+  },
+  onCollapseChange(e){
+    var that = this
+    that.setData({activeNames: e.detail})
+  }
+})
