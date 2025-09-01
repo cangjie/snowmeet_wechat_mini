09241a79318c26c8921f1b46350df9327c2346c2
cangjie <cangjie@hotmail.com>
2025-09-01 12:18:21 +0800

    rent item

---
+++ components/rent/rent_item_list.js
@@ -0,0 +1,24 @@
+// components/rent/rent_item_list.js
+Component({
+
+  /**
+   * Component properties
+   */
+  properties: {
+    rentals: Object
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
