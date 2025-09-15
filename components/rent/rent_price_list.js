94dca685ba5ce5ae55f619fe232cfe80a99ff027
cangjie <cangjie@hotmail.com>
2025-09-15 13:56:45 +0800

    rent price

--- components/rent/rent_price_list.js
+++ components/rent/rent_price_list.js
@@ -24,12 +24,12 @@ Component({
         data.getShopByNamePromise(that.properties.shopName).then(function (shop){
           that.setData({shop})
           that.getProductName()
-          data.getEnumListPromise('RentType').then(function(rentType){
-            that.setData({rentType})
-            data.getEnumListPromise('GetDayType').then(function (dayType){
-              that.setData({dayType})
-              data.getEnumListPromise('RentSceneType').then(function(RentSceneType){
-                that.setData({RentSceneType})
+          data.getEnumListPromise('RentType').then(function(rentTypeList){
+            that.setData({rentTypeList})
+            data.getEnumListPromise('GetDayType').then(function (dayTypeList){
+              that.setData({dayTypeList})
+              data.getEnumListPromise('RentSceneType').then(function(RentSceneTypeList){
+                that.setData({RentSceneTypeList})
                 if (that.properties.memberId){
                   that.setData({currentTag: 2})
                 }
@@ -37,8 +37,9 @@ Component({
                   that.setData({currentTag: 0})
                 }
                 var scene = that.data.RentSceneType[that.data.currentTag]
-                data.getRentPriceListPromise(that.data.shop.id, that.properties.type, that.properties.targetId, scene).then(function (matrix){
-                  that.setData({matrix})
+                data.getRentPriceListPromise(that.data.shop.id, that.properties.type, that.properties.targetId, scene).then(function (priceList){
+                  that.setData({priceist})
+                  that.listToMatrix(priceList)
                 })
               })
             })
@@ -70,10 +71,33 @@ Component({
       var that = this
       console.log('current tab', e)
       var scene = e.detail.title
-      data.getRentPriceListPromise(that.data.shop.id, that.properties.type, that.properties.targetId, scene).then(function (matrix){
-        that.setData({matrix})
+      data.getRentPriceListPromise(that.data.shop.id, that.properties.type, that.properties.targetId, scene).then(function (priceList){
+        that.setData({priceList})
+        that.listToMatrix(priceList)
       })
-    }
-    
+    },
+    listToMatrix(priceList){
+      var that = this
+      var matrix = []
+      for(var i = 0; i < that.data.rentTypeList.length; i++){
+        var line = []
+        for(var j = 0; j < that.data.dayTypeList.length; j++){
+          var cell = that.getPriceObject(priceList, that.data.dayTypeList[j], that.data.rentTypeList[i])
+          cell.priceStr = util.showAmount(cell.price)
+          line.push(cell)
+        }
+        matrix.push(line)
+      }
+      that.setData({matrix})
+    },
+    getPriceObject(priceList, dayType, rentType){
+      for(var i = 0; i < priceList.length; i++){
+        var price = priceList[i]
+        if (price.day_type == dayType && price.rent_type == rentType){
+          return price
+        }
+      }
+      return null
+    }    
   }
 })
