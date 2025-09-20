e897ca63f28e367384870709605c4d93d8d0ae60
苍杰 <cangjie@hotmail.com>
2025-09-20 21:55:17 +0800

    set domain

--- app.js
+++ app.js
@@ -133,8 +133,8 @@ App({
   },
   globalData: {
     appId: 'wxd1310896f2aa68bb',
-    domainName: 'mini.snowmeet.top',
-    requestPrefix: 'https://mini.snowmeet.top/api/',
+    domainName: 'snowmeet.wanlonghuaxue.com',
+    requestPrefix: 'https://snowmeet.wanlonghuaxue.com/api/',
     uploadDomain: 'xuexiaotupian.wanlonghuaxue.com',
     userInfo: null,
     sessionKey: '',
