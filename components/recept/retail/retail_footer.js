e6586aecd2641a71fb862582c13128a20862f338
cangjie <cangjie@hotmail.com>
2025-10-07 14:57:00 +0800

    retail start

---
+++ components/recept/retail/retail_footer.js
@@ -0,0 +1,24 @@
+// components/recept/retail/retail_footer.js
+Component({
+
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
---
+++ components/recept/retail/retail_recept.js
@@ -0,0 +1,24 @@
+// components/recept/retail/retail_recept.js
+Component({
+
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
