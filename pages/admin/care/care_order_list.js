eaf129c9c335770b0c4a9e918048e5b722f175c8
cangjie <cangjie@hotmail.com>
2025-11-08 17:50:07 +0800

    order list

--- pages/admin/care/care_order_list.js
+++ pages/admin/care/care_order_list.js
@@ -1,18 +1,29 @@
 // pages/admin/care/care_order_list.js
+const app = getApp()
+const util = require('../../../utils/util.js')
+const data = require('../../../utils/data.js')
 Page({
 
   /**
    * Page initial data
    */
   data: {
-
+    queryOptions:[
+      {key: 'isTest', value: false},
+      {key: 'isEntertain', value: false},
+      {key: 'haveDiscount', value: null}
+    ],
+    cell: null,
+    querying: false
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad(options) {
-
+    var that = this
+    var nowDate = new Date()
+    that.setData({startDate: util.formatDate(nowDate), endDate: util.formatDate(nowDate)})
   },
 
   /**
@@ -26,7 +37,11 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow() {
-
+    var that = this
+    app.loginPromiseNew.then(function (resolve){
+      that.setData({staff: app.globalData.staff, querying: true})
+      that.getData()
+    })
   },
 
   /**
@@ -62,5 +77,124 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+  getData(){
+    var that = this
+    var isTest = null
+    var isEntertain = null
+    var haveDiscount = null
+    var queryOptions = that.data.queryOptions
+    for(var i = 0; i < queryOptions.length; i++){
+      switch(queryOptions[i].key){
+        case 'isTest':
+          isTest = queryOptions[i].value
+          break
+        case 'isEntertain':
+          isEntertain = queryOptions[i].value
+          break
+        case 'haveDiscount':
+          haveDiscount = queryOptions[i].value
+          break
+        default:
+          break
+      }
+    }
+    var shop = that.data.shop
+    var startDate = that.data.startDate
+    var endDate = that.data.endDate
+    var cell = that.data.cell
+    if(cell != null && cell != ''){
+      startDate = new Date('2025-10-15')
+      endDate = null
+      shop = null
+      isTest = null
+      isEntertain = null
+      haveDiscount = null
+    }
+    data.getOrdersByStaffPromise(null, shop, null, null, '养护', startDate, endDate, 
+      null, isTest, isEntertain, null, null, haveDiscount, null, app.globalData.sessionKey, that.data.cell).then(function (orders){
+        console.log('get orders', orders)
+        that.renderOrders(orders)
+        that.setData({querying: false})
+      })
+  },
+  renderOrders(orders) {
+    var that = this
+    var totalAmount = 0
+    for (var i = 0; orders && i < orders.length; i++) {
+      var order = orders[i]
+      var bizDate = new Date(order.biz_date)
+      order.dateStr = util.formatDate(bizDate)
+      order.timeStr = util.formatTimeStr(bizDate)
+      totalAmount += order.totalCharge
+      order.totalChargeStr = util.showAmount(order.totalCharge)
+      var calledName = ''
+      var member = order.member
+      var cell = ''
+      var memberShip = '【散】'
+      if (order.contact_name) {
+        calledName = order.contact_name + (order.contact_gender == '男' ? '(先生)' : (order.contact_gender == '女' ? '(女士)' : ''))
+        cell = order.contact_num
+      }
+      else if (member) {
+        calledName = member.real_name + (member.gender == '男' ? ' 先生' : (member.gender == '女' ? ' 女士' : ''))
+        cell = member.cell
+      }
+      if (member && member.following_wechat == 1) {
+        memberShip = '【会】'
+      }
+      order.calledName = calledName
+      order.cell = cell
+      order.memberShip = memberShip
+      order.paidAmountStr = util.showAmount(order.paidAmount)
+      for (var j = 0; order.availablePayments && j < order.availablePayments.length; j++) {
+        order.payMethod = order.availablePayments[j].pay_method
+      }
+    }
+    that.setData({ orders, totalAmount })
+  },
+  query(){
+    var that = this
+    that.setData({querying: true})
+    that.getData()
+  },
+  setCell(e){
+    var that = this
+    //that.data.cell = e.detail.value
+    var cell = e.detail.value
+    that.setData({cell})
+  },
+  setQueryOptions(e){
+    var that = this
+    var id = e.currentTarget.id
+    that.setQueryOptionValue(id, e.detail.value)
+  },
+  setQueryOptionValue(key, value){
+    var that = this
+    var queryOptions = that.data.queryOptions
+    for(var i = 0; i < queryOptions.length; i++){
+      if (queryOptions[i].key == key){
+        switch(value){
+          case 'null':
+            queryOptions[i].value = null
+            break
+          case 'true':
+            queryOptions[i].value = true
+            break
+          case 'false':
+            queryOptions[i].value = false
+            break
+          default:
+            queryOptions[i].value = value
+            break
+        }
+      }
+    }
+    console.log('set query options', queryOptions)
+  },
+  shopSelected(e){
+    var that = this
+    console.log('shop selected', e)
+    that.setData({shop: e.detail.shop})
   }
 })
