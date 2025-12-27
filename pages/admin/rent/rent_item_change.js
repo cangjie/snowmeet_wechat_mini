a4f4d313367bd9abfb066b4cf34a5fb0f81f0b65
cangjie <cangjie@hotmail.com>
2025-12-28 00:33:32 +0800

    show ori rent item

--- pages/admin/rent/rent_item_change.js
+++ pages/admin/rent/rent_item_change.js
@@ -1,4 +1,7 @@
 // pages/admin/rent/rent_item_change.js
+const app = getApp()
+const util = require('../../../utils/util.js')
+const data = require('../../../utils/data.js')
 Page({
 
   /**
@@ -12,7 +15,9 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad(options) {
-
+    var that = this
+    that.data.orderId = options.orderId
+    that.data.rentItemId = options.rentItemId
   },
 
   /**
@@ -26,7 +31,18 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow() {
-
+    var that = this
+    data.getOrderByStaffPromise(that.data.orderId, app.globalData.sessionKey).then(function (order){
+      that.data.order = order
+      var rentItem = that.getRentItem(that.data.rentItemId)
+      console.log('get rent item', rentItem)
+      rentItem = that.renderRentItem(rentItem)
+      data.getRentCategoryPromise(rentItem.category_id).then(function (category){
+        rentItem.category = category
+        that.setData({rentItem})
+      })
+      //that.setData({rentItem})
+    })
   },
 
   /**
@@ -62,5 +78,26 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+  getRentItem(id){
+    var that = this
+    var order = that.data.order
+    var item = null
+    for(var i = 0; item == null && i < order.rentals.length; i++){
+      var rental = order.rentals[i]
+      for(var j = 0; item== null && j < rental.rentItems.length; j++){
+        if (rental.rentItems[j].id == that.data.rentItemId){
+          item = rental.rentItems[j]
+        }
+      }
+    }
+    return item
+  },
+  renderRentItem(item){
+    var pickDate = new Date(item.pickDate)
+    item.pickDateDateStr = util.formatDate(pickDate)
+    item.pickDateTimeStr = util.formatTimeStr(pickDate)
+    return item
   }
+  
 })
