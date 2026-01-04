80b583c492cdcaf2536d427ef9b5ccb5f8e2096e
cangjie <cangjie@hotmail.com>
2026-01-04 23:46:44 +0800

    show balance

--- components/payment/balance.js
+++ components/payment/balance.js
@@ -19,7 +19,33 @@ Component({
   },
   lifetimes:{
     ready(){
+      var that = this
+      var orderId = that.properties.orderId
+      if (!isNaN(orderId)){
+        data.getOrderBalancePromise(orderId, app.globalData.sessionKey).then(function (balances){
+          for(var i = 0; balances != null && i < balances.length; i++){
+            var balance = balances[i]
+            var transDate = new Date(balance.transDate)
+            balance.transDateStr = util.formatDate(transDate)
+            balance.transTimeStr = util.formatTimeStr(transDate)
+            balance.amountStr = util.showAmount(balance.amount)
+            switch (balance.transType){
+              case '支付':
+                balance.color = 'green'
+                break
+              case '退款' :
+                balance.color = 'red'
+                break
+              default:
+                break
+            }
+          }
+          that.setData({balances})
+        })
+      }
+      
 
+      
     }
   },
 
