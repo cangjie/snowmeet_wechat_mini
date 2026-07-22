b466472a4f12b62be15904773c6bbf603cb77ed3
cangjie <cangjie@hotmail.com>
2026-07-22 13:18:56 +0800

    remove unused files

--- automation_test/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
-  // 启动开发者工具 + 编译需要时间，单条用例给足超时
-  testTimeout: 120000,
-  // 串行执行，避免多个用例同时抢占开发者工具
-  maxWorkers: 1,
-}
